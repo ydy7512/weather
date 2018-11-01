24003240c9e8175addbc59ebbf96546841ce699f
Young <972404058@qq.com>
2018-11-01 07:00:16 +0000

    Test StyleCI.

--- src/Weather.php
+++ src/Weather.php
@@ -48,8 +48,7 @@ class Weather
      * @throws InvalidArgumentException
      */
     public function getWeather($city, $type = 'base', $format = 'json')
-    {
-        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+    {$url = 'https://restapi.amap.com/v3/weather/weatherInfo';
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$format);
         }
